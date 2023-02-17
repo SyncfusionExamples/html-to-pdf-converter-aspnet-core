de1eef1d79a7d4ba233760d69db61464c5609ea6
jeyalakshmit <jeyalakshmi.t@syncfusion.com>
2023-02-17 18:09:48 +0530

    EJDOTNETCORE-4511 Need to create a sample for HTML to PDF conversion in ASP.NET Core

---
+++ HTMLToPDFSample/HTMLToPDFSample/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using HTMLToPDFSample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Syncfusion.HtmlConverter;
+using Syncfusion.Pdf;
+
+namespace HTMLToPDFSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        //<summary>
+        ///Gets or sets DateTime.
+        ///</summary>
+        private DateTime DateTime { get; set; }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Invoice()
+        {
+            var nwDt = DateTime.Now.ToString("MM/dd/yyyy");
+            ViewData["nowDt"] = nwDt;
+            var nxtDt = DateTime.Now.AddMonths(1).ToString("MM/dd/yyyy");
+            ViewData["nextDt"] = nxtDt;
+            return View();
+        }
+        public ActionResult Index()
+        {   
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult ExportToPDF()
+        {
+            HtmlToPdfConverter converter = new HtmlToPdfConverter();
+            BlinkConverterSettings blinkConverterSettings = new BlinkConverterSettings();
+            blinkConverterSettings.ViewPortSize = new Syncfusion.Drawing.Size(800, 0);
+            converter.ConverterSettings = blinkConverterSettings;
+            using (PdfDocument document = converter.Convert("https://localhost:7156/Home/Invoice"))
+            {
+                MemoryStream stream = new MemoryStream();
+                document.Save(stream);
+                stream.Position = 0;
+                return File(stream, "application/pdf", "HTML-to-PDF.pdf");
+            }
+        }
+                
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# html-to-pdf-converter-aspnet-core
-This repository contains code that demonstrates how to use Syncfusion's HTML to PDF Conversion library to convert HTML files to PDF documents in an ASP.NET Core application. 
+# Convert HTML to PDF in ASP.NET Core application
+
+## Introduction
+The Syncfusion HTML to PDF converter is a .NET library used to convert HTML or web pages to PDF document in ASP.NET Core application.
+
+## System requirement
+### Framework and SDKs:
+    * .NET Core SDK (version 3.1 or later)
+
+### IDEs:
+    *	Visual Studio 2019/ Visual Studio 2022 
+    *	Visual Studio Code 
+
+## Code snippet to convert HTML to PDF
+we will create a existing ASP.Net Core application, add the Syncfusion HTML to PDF library package, and write the code
+
+```csharp
+    HtmlToPdfConverter converter = new HtmlToPdfConverter();
+    BlinkConverterSettings blinkConverterSettings = new BlinkConverterSettings();
+    blinkConverterSettings.ViewPortSize = new Syncfusion.Drawing.Size(800, 0);
+    converter.ConverterSettings = blinkConverterSettings;
+    using (PdfDocument document = converter.Convert("https://localhost:7156/Home/Invoice"))
+    {
+        MemoryStream stream = new MemoryStream();
+        document.Save(stream);
+        stream.Position = 0;
+        return File(stream, "application/pdf", "HTML-to-PDF.pdf");
+    }
+```
+ **Invoice URL Image**
+<img src="HTMLToPDFSample/HTMLToPDFSample/Output_images/Invoice_url_image.png" alt="output_image" width="100%" Height="Auto"/>
+
+**Output Image**
+<img src="HTMLToPDFSample/HTMLToPDFSample/Output_images/HTML-to-PDF_Output.Jpeg" alt="output_image" width="100%" Height="Auto"/>
